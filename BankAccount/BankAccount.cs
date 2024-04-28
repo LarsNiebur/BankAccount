da6a95d2301bb50d681c89d497e3e877d2007832
MrPink <larsniebur78@gmail.com>
2024-04-28 10:33:38 +0200

    Projektdateien hinzufügen.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        public string Number  { get; private set; }
+        public string Owner   { get; private set; }
+        public float  Balance { get; private set; }
+
+        public BankAccount( string number, string owner, float initialBalance ) 
+        { 
+            this.Number  = number;
+            this.Owner   = owner;
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit( float amount )
+        {
+            this.Balance += amount;
+        }
+
+        public void MakeWithdrawal ( float amount ) 
+        {
+            if ( amount <= this.Balance )
+            {
+                this.Balance -= amount;
+            } 
+            else
+            {
+                Console.WriteLine("Unzureichender Saldo");
+            }
+        }
+
+        public void PrintBalance ()
+        {
+            Console.WriteLine($"Kontostand von {this.Owner}: {this.Balance}");
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Geben Sie Ihren Namen ein:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Bitte Geben Sie Ihre Kontonummer ein:");
+            string number = Console.ReadLine();
+
+            Console.WriteLine("Bitte Geben Sie Ihren Kontostand ein:");
+            float  initialBalance = float.Parse(Console.ReadLine());
+
+            BankAccount account = new BankAccount(
+                                                    number,
+                                                    name,
+                                                    initialBalance
+                                                  );
+
+            while (true)
+            {
+                Console.WriteLine("Bitte wählen Sie iene Aktion aus:");
+                Console.WriteLine("1. Kontostand anzeigen");
+                Console.WriteLine("2. Einzahlung machen");
+                Console.WriteLine("3. Auszahlung machen");
+                Console.WriteLine("4. Beenden");
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    account.PrintBalance();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Bitte geben Sie den Betrag ein den Sie einzahlen möchten: ");
+                    float depositAmount = float.Parse(Console.ReadLine());
+                    account.MakeDeposit(depositAmount);
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("Bitte geben Sie den Betrag ein den Sie anheben möchten: ");
+                    float withdrawlAmount = float.Parse(Console.ReadLine());
+                    account.MakeWithdrawal(withdrawlAmount);
+                }
+                else if (input == "4")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe!");
+                }
+            }
+        }
+    }
+}
